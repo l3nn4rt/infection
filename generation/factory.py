77e6e466bbaf52fdc69d0b514d10890c008d0dff
l3nn4rt <l3nn4rt@protonmail.com>
2021-03-21 23:05:40 +0100

    generation: prepare for argument parser
    This commit introduces a structured definition of template arguments,
    that can be used to instruct a smart argument parser later on.
    
    Each enum in `infection.generation.factory.Factory.Template` now has:
    - brief description
    - arguments with:
        - human-friendly description
        - type
        - domain validator
    
    The method `infection.generation.factory.Factory.build` will now raise
    helpful errors if the arguments are not correct too (some unhandled
    errors will still be raised by networkx).

--- generation/factory.py
+++ generation/factory.py
@@ -1,108 +1,145 @@
 import enum
 import random
-import textwrap
 import typing
 
 import networkx as nx
 
+
+def _build_cycle(n: int):
+    return nx.cycle_graph(n)
+
+
+def _build_erdos_renyi(n: int, p: float):
+    return nx.erdos_renyi_graph(n, p)
+
+
+def _build_cycle_erdos_renyi(n: int, p: float):
+    return nx.compose(
+            nx.cycle_graph(n),
+            nx.erdos_renyi_graph(n, p))
+
+
+def _build_cycle_perfect_matching(n: int):
+    g = nx.cycle_graph(n)
+    # a valid matching is the diameters list
+    matching = [(i, (i+n//2)%n) for i in range(n//2)]
+    edge_indexes = list(range(len(matching)))
+    # for n times, try to swap two random edges' ends
+    for _ in range(n):
+        i, j = random.sample(edge_indexes, 2)
+        (a, b), (c, d) = matching[i], matching[j]
+        # skip if (a,d) or (b,c) already exist
+        if not a in g[d] and not b in g[c]:
+            matching[i], matching[j] = (a, d), (c, b)
+    g.add_edges_from(matching)
+    #assert all(d == 3 for d in dict(g.degree).values())
+
+    return g
+
+
+def _build_torus(m: int, n: int):
+    g = nx.grid_2d_graph(m, n, True)
+    g = nx.relabel_nodes(g, {n:i for i,n in enumerate(g.nodes)})
+    return g
+
+
 class Factory:
 
     class Template(enum.Enum):
-        CYCLE = textwrap.dedent("""
-                Graph whose nodes are connected in sequence; last
-                node is connected to the first.
 
-                Required keywords:
-                - n: number of nodes (non-negative integer).
-                """)
-        CYCLE_U_ERDOS_RENYI = textwrap.dedent("""
-                Union of a cycle graph and an Erdos-Renyi graph.
+        CYCLE = {
+            'help': "Graph whose nodes are connected in sequence; last node " \
+                    "is connected to the first.",
+            'vars': {
+                'n': {
+                    'help': 'number of nodes (non-negative integer)',
+                    'type': int,
+                    'test': lambda n: n >= 0
+                }
+            },
+            'builder': _build_cycle
+        }
 
-                Required keywords:
-                - n: number of nodes (non-negative integer).
-                - p: edges probability (float in [0,1]).
+        ERDOS_RENYI = {
+            'help': "Graph whose each pair of nodes is an edge with " \
+                    "a given probability",
+            'vars': {
+                'n': {
+                    'help': 'number of nodes (non-negative integer)',
+                    'type': int,
+                    'test': lambda n:  n >= 0
+                },
+                'p': {
+                    'help': 'edges probability (float in [0,1])',
+                    'type': float,
+                    'test': lambda p: 0 <= p <= 1
+                }
+            },
+            'builder': _build_erdos_renyi
+        }
 
-                Note: the probability p does not affect the cycle subgraph.
-                """)
-        CYCLE_U_PERFECT_MATCHING = textwrap.dedent("""
-                Union of a cycle graph and a perfect matching graph.
+        CYCLE_U_ERDOS_RENYI = {
+            'help': "Union of a cycle graph and an Erdos-Renyi graph.",
+            'vars': {
+                'n': {
+                    'help': 'number of nodes (non-negative integer)',
+                    'type': int,
+                    'test': lambda n: n >= 0
+                },
+                'p': {
+                    'help': 'edges probability (float in [0,1])',
+                    'type': float,
+                    'test': lambda p: 0 <= p <= 1
+                }
+            },
+            'builder': _build_cycle_erdos_renyi
+        }
 
-                Required keywords:
-                - n: number of nodes (even, non-negative integer).
-                """)
-        ERDOS_RENYI = textwrap.dedent("""
-                Graph whose each pair of nodes is an edge with a given
-                probability.
+        CYCLE_U_PERFECT_MATCHING = {
+            'help': "Union of a cycle graph and a perfect matching graph.",
+            'vars': {
+                'n': {
+                    'help': 'number of nodes (even, non-negative integer)',
+                    'type': int,
+                    'test': lambda n: n >= 0 and n % 2 == 0
+                }
+            },
+            'builder': _build_cycle_perfect_matching
+        }
 
-                Required keywords:
-                - n: number of nodes (non-negative integer).
-                - p: edges probability (float in [0,1]).
-                """)
-        TORUS = textwrap.dedent("""
-                Graph whose nodes are arranged in a 2-dimensional lattice,
-                wrapped along both axes; each node has four neighbors.
+        TORUS = {
+            'help': "Graph whose nodes are arranged in a 2-dimensional lattice, " \
+                    "wrapped along both axes; each node has four neighbors",
+            'vars': {
+                'm': {
+                    'help': 'first dimension (non-negative integer)',
+                    'type': int,
+                    'test': lambda n: n >= 0
+                },
+                'n': {
+                    'help': 'second dimension (non-negative integer)',
+                    'type': int,
+                    'test': lambda n: n >= 0
+                }
+            },
+            'builder': _build_torus
+        }
 
-                Required keywords:
-                - m: first dimension (non-negative integer).
-                - n: second dimension (non-negative integer).
-                """)
 
     def build(self, template: Template, **kwargs)-> typing.Optional[nx.Graph]:
-        if template == __class__.Template.CYCLE:
-            assert 'n' in kwargs
-            n = kwargs['n']
-            assert isinstance(n, int) and n >= 0
+        """Build graph from template."""
+        templ_vars = template.value['vars']
 
-            return nx.cycle_graph(n)
+        for tv in templ_vars:
+            info = templ_vars[tv]['help']
 
-        if template == __class__.Template.TORUS:
-            assert 'm' in kwargs
-            assert 'n' in kwargs
-            m, n = kwargs['m'], kwargs['n']
-            assert isinstance(m, int) and m >= 0
-            assert isinstance(n, int) and n >= 0
+            if not tv in kwargs:
+                raise KeyError(f"{tv} not found - {info}")
 
-            g = nx.grid_2d_graph(m, n, True)
-            g = nx.relabel_nodes(g, {n:i for i,n in enumerate(g.nodes)})
-            return g
+            if not isinstance(kwargs[tv], templ_vars[tv]['type']):
+                raise TypeError(f"{tv} wrong type - {info}")
 
-        if template == __class__.Template.ERDOS_RENYI:
-            assert 'n' in kwargs
-            assert 'p' in kwargs
-            n, p = kwargs['n'], kwargs['p']
-            assert isinstance(n, int) and n >= 0
-            assert isinstance(p, float) and 0 <= p <= 1
+            if not templ_vars[tv]['test'](kwargs[tv]):
+                raise ValueError(f"{tv} invalid value - {info}")
 
-            return nx.erdos_renyi_graph(n, p)
-
-        if template == __class__.Template.CYCLE_U_ERDOS_RENYI:
-            assert 'n' in kwargs
-            assert 'p' in kwargs
-            n, p = kwargs['n'], kwargs['p']
-            assert isinstance(n, int) and n >= 0
-            assert isinstance(p, float) and 0 <= p <= 1
-
-            return nx.compose(
-                    self.build(__class__.Template.CYCLE, n=n),
-                    self.build(__class__.Template.ERDOS_RENYI, n=n, p=p))
-
-        if template == __class__.Template.CYCLE_U_PERFECT_MATCHING:
-            assert 'n' in kwargs
-            n = kwargs['n']
-            assert isinstance(n, int) and n >= 0 and n % 2 == 0
-
-            g = self.build(__class__.Template.CYCLE, n=n)
-            # a valid matching is the diameters list
-            matching = [(i, (i+n//2)%n) for i in range(n//2)]
-            edge_indexes = list(range(len(matching)))
-            # for n times, try to swap two random edges' ends
-            for _ in range(n):
-                i, j = random.sample(edge_indexes, 2)
-                (a, b), (c, d) = matching[i], matching[j]
-                # skip if (a,d) or (b,c) already exist
-                if not a in g[d] and not b in g[c]:
-                    matching[i], matching[j] = (a, d), (c, b)
-            g.add_edges_from(matching)
-            #assert all(d == 3 for d in dict(g.degree).values())
-
-            return g
+        return template.value['builder'](**kwargs)
