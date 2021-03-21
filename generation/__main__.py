81b18a2b9bd656e0ae66f64a32374fef65037019
l3nn4rt <l3nn4rt@protonmail.com>
2021-03-21 23:01:35 +0100

    generation: fix erdos-renyi template

--- generation/__main__.py
+++ generation/__main__.py
@@ -43,7 +43,7 @@ def main():
         mkdir_p(er_dir)
         nx.write_adjlist(er, os.path.join(er_dir, 'graph.adjlist'))
 
-        cer = f.build(Factory.Template.CYCLE_U_PERFECT_MATCHING, n=n, p=p)
+        cer = f.build(Factory.Template.CYCLE_U_ERDOS_RENYI, n=n, p=p)
         cer_dir = os.path.join(SAMPLES_DIR, 'cycle-plus-erdos-renyi-%d-%0.2f' % (n, p))
         mkdir_p(cer_dir)
         nx.write_adjlist(cer, os.path.join(cer_dir, 'graph.adjlist'))
