28f7ec11a9fec94b32d481fc3d9721dae01a130b
l3nn4rt <l3nn4rt@protonmail.com>
2021-03-23 15:08:55 +0100

    visualization: create timeline class

---
+++ visualization/timeline.py
@@ -0,0 +1,23 @@
+import math
+
+from infection.node import NodeState
+
+
+class Timeline:
+
+    def __init__(self, nodes, rounds) -> None:
+        self.lines = []
+        prefix_width = math.ceil(math.log10(len(rounds) + 1))
+        for round_idx, round_dict in enumerate(rounds):
+            line_lst = ['[%*d] ' % (prefix_width, round_idx)]
+            for label in nodes:
+                if label in round_dict['infectious']:
+                    line_lst.append(NodeState.INFECTIOUS.value['cli_str'])
+                elif label in round_dict['recovered']:
+                    line_lst.append(NodeState.RECOVERED.value['cli_str'])
+                else:
+                    line_lst.append(NodeState.SUSCEPTIBLE.value['cli_str'])
+            self.lines.append(''.join(line_lst))
+
+    def __str__(self):
+        return '\n'.join(self.lines)
