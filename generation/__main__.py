2437110448e55267d15e584375aadcda439a1e92
l3nn4rt <l3nn4rt@protonmail.com>
2021-04-20 20:59:54 +0200

    generation: add options for saving graph
    With options `--save` the graph is saved in `./graphs`; this directory
    can be changed with option `--graph-dir`.

--- generation/__main__.py
+++ generation/__main__.py
@@ -5,14 +5,23 @@ Generate graphs.
 """
 
 import argparse
+import hashlib
+import os.path
 
 import networkx as nx
 
 from . import *
+from .. import util
 
 
 def main():
     parser = argparse.ArgumentParser(prog=__package__, description=__doc__)
+    parser.add_argument('--graph-dir', metavar='PATH', help="""Graph directory
+            path; this is created when needed. By default, use 'graphs' in the
+            working directory).""", default='graphs', type=str)
+    parser.add_argument('--save', help="""Save graph adjacency list in graph
+            directory and return graph UID (file hash). See also '--graph-dir'
+            for more info.""", action='store_true')
     subparsers = parser.add_subparsers(metavar= 'TEMPLATE',
             help="""Generate graph using TEMPLATE template. Available templates are:
             """ + str([*Factory.Template.__members__])[1:-1], required=True)
@@ -40,9 +49,25 @@ def main():
 
     # create graph
     g = Factory().build(templ, **templ_kwargs)
+    txt = '\n'.join(nx.generate_adjlist(g)) + '\n'
 
-    for line in nx.generate_adjlist(g):
-        print(line)
+    if args.save:
+        # we don't use nx.write_adjlist() because it adds a commment and this
+        # behaviour can't be avoided; instead we create a file whose content
+        # is the same text that would be printed on stdout
+        file_hash = hashlib.sha1(txt.encode()).hexdigest()
+        file_path = os.path.join(args.graph_dir, file_hash)
+
+        try:
+            util.mkdir_p(args.graph_dir)
+            with open(file_path, 'w') as f:
+                f.write(txt)
+        except (NotADirectoryError, PermissionError) as e:
+            util.die(__package__, e)
+
+        print('Graph UID:', file_hash)
+    else:
+        print(txt, end='')
 
 if __name__ == "__main__":
     main()
